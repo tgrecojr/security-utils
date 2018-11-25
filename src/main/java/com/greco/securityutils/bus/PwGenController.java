310822d352c514f151274a2a3824da21f59f17dd
T.J. Greco <tgrecojr@gmail.com>
2018-11-25 07:37:47 -0500

    using Optional API to get parameters for passwordType

--- src/main/java/com/greco/securityutils/bus/PwGenController.java
+++ src/main/java/com/greco/securityutils/bus/PwGenController.java
@@ -16,18 +16,13 @@ import java.util.Optional;
 @RestController
 public class PwGenController {
 
-    private static char[]  ALL_CHARACTERS =  { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-            'U', 'V', 'W', 'X', 'Y', 'Z', '@','%','+','\\','/','\'','!','#','$','^','?',':','.','(',')','{','}','[',']','~','-','_','.',
-            '1','2','3','4','5','6','7','8','9','0'};
-
-    private static char[]  ALPHABETIC = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    private static char[]  ALLOWED_VALUES_ALPHABETIC = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
             'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
             'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-    private static char[]  NUMERIC = { '1','2','3','4','5','6','7','8','9','0'};
+    private static char[]  ALLOWED_VALUES_NUMERIC = { '1','2','3','4','5','6','7','8','9','0'};
 
-    private static char[] SYMBOLS = { '@','%','+','\\','/','\'','!','#','$','^','?',':','.','(',')','{','}','[',']','~','-','_','.'};
+    private static char[] ALLOWED_VALUES_SYMBOLS = { '@','%','+','\\','/','\'','!','#','$','^','?',':','.','(',')','{','}','[',']','~','-','_','.'};
 
     private static String TYPE_ALPHANUMERIC = "alphanumeric";
     private static String TYPE_ALPHABETIC = "alphabetic";
@@ -36,30 +31,26 @@ public class PwGenController {
     @GetMapping("/pwgen")
     private String generatePassword(@RequestParam Optional<String> type, @RequestParam int passwordLength) throws Exception{
 
-        char[] allowedValues;
-        log.info("TYPE PRESENT: " + type.isPresent() );
-        log.info("TYPE: " + type.get() );
-        if (type.isPresent()){
-            if (type.get().equals(TYPE_ALPHABETIC)){
-                log.info("USING ALPHABETIC SET");
-                allowedValues = ALPHABETIC;
-            }else if(type.get().equals(TYPE_ALPHANUMERIC)){
-                log.info("USING ALPHANUMERIC SET");
-                allowedValues = Chars.concat(ALPHABETIC,NUMERIC);
-            }else{
-                log.info("USING DEFAULT SET");
-                allowedValues = Chars.concat(ALPHABETIC,NUMERIC,SYMBOLS);
-            }
-        }else{
-            allowedValues = Chars.concat(ALPHABETIC,NUMERIC,SYMBOLS);
-        }
         SecureRandom random = SecureRandom.getInstanceStrong();
         RandomStringGenerator generator = new RandomStringGenerator.Builder()
-                .selectFrom(allowedValues)
+                .selectFrom(getAllowedValues(type))
                 .usingRandom(random::nextInt)
                 .build();
         String randomLetters = generator.generate(passwordLength);
         return randomLetters;
+
+    }
+
+    protected char[] getAllowedValues(Optional<String> passwordType){
+        boolean isAlphabetic = passwordType.filter(type -> type.equals(TYPE_ALPHABETIC)).isPresent();
+        boolean isAlphaNumeric = passwordType.filter(type -> type.equals(TYPE_ALPHANUMERIC)).isPresent();
+        if(isAlphabetic){
+            return ALLOWED_VALUES_ALPHABETIC;
+        }else if(isAlphaNumeric){
+            return Chars.concat(ALLOWED_VALUES_ALPHABETIC, ALLOWED_VALUES_NUMERIC);
+        }else{
+            return Chars.concat(ALLOWED_VALUES_ALPHABETIC, ALLOWED_VALUES_NUMERIC, ALLOWED_VALUES_SYMBOLS);
+        }
 
     }
 }
